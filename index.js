fa99c8409895c7b7994429ef75e75229605f1104
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-04-28 18:01:33 -0600

    Created Pseudo Code for project and installed Inquirer@8.2.4 and Jest DevDependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+// Step 1. Create a prompt for user to fill criteria of SVG logo maker
+// Step 2. Create Class for Triangle shape
+// Step 2.A Add appropriate values for triangle class (string).
+// Step 3. Create Class for Circle shape
+// Step 3.A Add appropriate values for Circle class (string).
+// Step 4. Create Class for Square shape
+// Step 4.A Add appropriate values for Square class (string).
+// Step 5. Validate data within prompt
+// Step 6. Create atleast 3 tests for every file in lib directory
+// Step 7. Generate required data based on classes 
+// Step 8. Create a writeFile which will create the SVG based on prompt and console log "Generated logo.svg"
+
+
+
