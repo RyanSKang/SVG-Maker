e593371d0148b815fe56a9b8b6dd88673f98acf6
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-04-30 16:29:44 -0600

    Created prompt for user in Index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Include packages needed to run application
+const inquirer = require("inquirer");
+const {Triangle, Circle, Square} = require("/lib/shapes.js")
+
 // Step 1. Create a prompt for user to fill criteria of SVG logo maker
 // Step 2. Create Class for Triangle shape
 // Step 2.A Add appropriate values for triangle class (string).
@@ -11,4 +15,32 @@
 // Step 8. Create a writeFile which will create the SVG based on prompt and console log "Generated logo.svg"
 
 
-
+// Step 1. Create a prompt for user to fill criteria of SVG logo maker
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter up to 3 characters.',
+        name: 'character',
+    },
+    {
+        type: 'input',
+        message: 'Enter a color keyword for the text.',
+        name: 'text-color',
+    },
+    {
+        type: 'input',
+        message: 'Enter a color keyword for the shape.',
+        name: 'shape-color',
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like for your logo?',
+        choicees: 
+        [
+            'Triangle',
+            'Circle',
+            'Square'
+        ]
+    }
+];
