c927c30966521f00a0229fa222f94a146d81f705
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-05-01 20:49:46 -0600

    Added validation for prompt and shapes

--- index.js
+++ index.js
@@ -62,3 +62,66 @@ const questions = [
         ]
     }
 ];
+
+// Step 8. Create a writeFile which will create the SVG based on prompt and console log "Generated logo.svg"
+function writeFile(name, data){
+    console.log(`Writing ${data} to file ${name}`);
+    gracefulFS.writeFile(name, data, function (err){
+        if (err){
+            return console.log(err);
+        }
+        console.log("Generated logo.svg");
+    })
+}
+
+async function init() {
+    console.log("init initializing")
+    var string="";
+    var file="GeneratedLogo.svg";
+
+    // Creating prompt from questions object
+    const response= await inquirer.prompt(questions);
+
+    // Step 5. Validate data within prompt (3 Character limit)
+    var userAns="";
+    if (response.text.length > 0 && response.text.length <4){
+        userAns=response.text;
+    } else {
+        console.log ("Invalid response in text field, Need 1-3 characters ONLY!")
+    }
+    // Console log out the userAns
+    console.log(`User's Character(s): ${userAns}`)
+
+    // Console log out the text color
+    console.log(`Text color: ${textColor}`)
+
+
+    // Step 5. Validate data within shape  (3 Character limit)
+let userShapeEl;
+    if(userShape === "Triangle"){
+        userShape= new Triangle();
+        console.log("You selected Triangle!")
+    }
+    else if(userShape === "Cirlce"){
+        userShape= new Circle();
+        console.log("You selected Circle!")
+    }
+    else if(userShape === "Square"){
+        userShape= new Square();
+        console.log("You selected Square!")
+    }
+    else {
+        console.log("Please select shape from the following options.")
+    }
+    userShape.setColor(shapeColor)
+
+    // Console log out the shape
+    console.log(`Shape: ${shapeEl}`)
+
+    // console log out the shape color
+    console.log(`Shape color: ${shapeColor}`)
+
+
+
+
+}
