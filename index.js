a4cd603c1e9688e6bbbbf931e870366f5fc2aa94
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-24 13:20:02 -0600

    able to render a file and character but not the shape or shape color

--- index.js
+++ index.js
@@ -86,52 +86,57 @@ async function init() {
     // Step 5. Validate data within prompt (3 Character limit)
     var userAns="";
     if (response.character.length > 0 && response.character.length <4){
-        userAns=response.text;
+        userAns=response.character;
     } else {
-        console.log ("Invalid response in text field, Need 1-3 characters ONLY!")
+        console.log ("Invalid response in text field, Need 1-3 characters ONLY!");
+        return;
     }
     // Console log out the userAns
     console.log(`User's Character(s): ${userAns}`)
 
     // Console log out the text color
-    console.log(`Text color: ${textColor}`)
+    const textColor = response['text-color'];
+    console.log(`Text color: ${textColor}`);
+
+    // Console log out the shape color
+    const shapeColor= response['shape-color'];
+    console.log(`Shape color: ${shapeColor}`);
+
+    // Console log out the shape
+    const shape= response.shape;
+    console.log(`Shape: ${shape}`);
 
 
     // Step 5. Validate data within shape  (3 Character limit)
 let userShapeEl;
-    if(userShape === "Triangle"){
-        userShape= new Triangle();
+    if(shape === "Triangle" || shape === "triangle"){
+        userShapeEl= new Triangle();
         console.log("You selected Triangle!")
     }
-    else if(userShape === "Cirlce"){
-        userShape= new Circle();
+    else if(shape === "Circle" || shape === "circle"){
+        userShapeEl= new Circle();
         console.log("You selected Circle!")
     }
-    else if(userShape === "Square"){
-        userShape= new Square();
+    else if(shape === "Square" || shape === "square"){
+        userShapeEl= new Square();
         console.log("You selected Square!")
     }
     else {
-        console.log("Please select shape from the following options.")
+        console.log("Error! Please select shape from the following options.")
     }
-    userShape.setColor(shapeColor)
-
-    // Console log out the shape
-    console.log(`Shape: ${shapeEl}`)
-
-    // console log out the shape color
-    console.log(`Shape color: ${shapeColor}`)
+    userShapeEl.setColor(shapeColor)
 
 // Create a new SVG class with the new shape and text element
 var generatedSVG= new SVG();
     generatedSVG.setTextElement(userAns, textColor);
-    generatedSVG.SetShapeElement(shapeEl);
+    generatedSVG.SetShapeElement(userShapeEl);
     string=generatedSVG.render();
 
 // Print shape to log 
 console.log("Displaying: \n" + string)
 console.log("Generated logo.svg Complete");
-writetoFile(file, string)
-}
+writeFile(file, string)
+
+};
 
 init();
